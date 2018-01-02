b2e8eac7d880e4511ed955f3373b640962fffdb6
sechix <sechix@gmail.com>
2018-01-02 17:27:21 +0100

    errors

--- app/assets/javascripts/store/spree_infinite_scroll.js
+++ app/assets/javascripts/store/spree_infinite_scroll.js
@@ -1,14 +1,17 @@
-//= require store/spree_frontend
 
-$(function() {
-  $('nav.pagination').hide();
-  $('#products').infinitescroll({
-    navSelector: "nav.pagination",
-    nextSelector: "nav.pagination a[rel=next]",
-    itemSelector: "#products li",
-    loading: {
-      msgText  : "cargando...",
-      finishedMsg: ""  
-    }
-  });
+$(document).ready(function () {
+
+    $(function() {
+      $('.pagination').hide();
+      $('#products').infinitescroll({
+        navSelector: ".pagination",
+        nextSelector: ".pagination a[rel=next]",
+        itemSelector: "#products li",
+        loading: {
+          msgText  : "cargando...",
+          finishedMsg: ""
+        }
+      });
+    });
+
 });
