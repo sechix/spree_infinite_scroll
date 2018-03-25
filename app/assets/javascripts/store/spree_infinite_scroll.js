7d0165570d7cc1772a91d7e0668f406d8f78a077
sechix <sechix@gmail.com>
2018-03-25 21:53:05 +0200

    improve to last version of infinite-scroll

--- app/assets/javascripts/store/spree_infinite_scroll.js
+++ app/assets/javascripts/store/spree_infinite_scroll.js
@@ -1,17 +1,25 @@
 
 $(document).ready(function () {
 
-    $(function() {
-      $('.pagination').hide();
-      $('#products').infinitescroll({
-        navSelector: ".pagination",
-        nextSelector: ".pagination a[rel=next]",
-        itemSelector: "#products li",
-        loading: {
-          msgText  : "cargando...",
-          finishedMsg: ""
-        }
-      });
-    });
+      // $('.pagination').hide();
+
+        $('#products').infiniteScroll({
+            path: '.pagination a[rel=next]',
+            append: '#products li',
+            status: '.scroller-status',
+            hideNav: '.pagination',
+        });
+
+
+      // $('#products').infinitescroll({
+      //   animate: true,
+      //   navSelector: ".pagination",
+      //   nextSelector: ".pagination a[rel=next]",
+      //   itemSelector: "#products li",
+      //   loading: {
+      //     msgText  : "cargando...",
+      //     finishedMsg: ""
+      //   }
+      // });
 
 });
