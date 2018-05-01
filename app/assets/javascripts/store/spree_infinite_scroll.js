ff56f61bbfb49131f7716c399f1ee017c17f1191
sechix <sechix@gmail.com>
2018-05-01 15:07:32 +0200

    New version that execute scripts inside

--- app/assets/javascripts/store/spree_infinite_scroll.js
+++ app/assets/javascripts/store/spree_infinite_scroll.js
@@ -1,25 +1,25 @@
 
 $(document).ready(function () {
 
-      // $('.pagination').hide();
+      $('.pagination').hide();
 
-        // $('#products').infiniteScroll({
-        //     path: '.pagination a[rel=next]',
-        //     append: '#products li',
-        //     status: '.scroller-status',
-        //     hideNav: '.pagination',
-        // });
+        $('#products').infiniteScroll({
+            path: '.pagination a[rel=next]',
+            append: '#products li',
+            status: '.scroller-status',
+            hideNav: '.pagination',
+        });
 
 
-      // $('#products').infinitescroll({
-      //   animate: true,
-      //   navSelector: ".pagination",
-      //   nextSelector: ".pagination a[rel=next]",
-      //   itemSelector: "#products li",
-      //   loading: {
-      //     msgText  : "cargando...",
-      //     finishedMsg: ""
-      //   }
-      // });
+      $('#products').infinitescroll({
+        animate: true,
+        navSelector: ".pagination",
+        nextSelector: ".pagination a[rel=next]",
+        itemSelector: "#products li",
+        loading: {
+          msgText  : "cargando...",
+          finishedMsg: ""
+        }
+      });
 
 });
