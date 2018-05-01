6696798cc47793afe925169ca1b69fd5b3a3d201
sechix <sechix@gmail.com>
2018-05-01 16:38:41 +0200

    duplicate call

--- app/assets/javascripts/store/spree_infinite_scroll.js
+++ app/assets/javascripts/store/spree_infinite_scroll.js
@@ -11,15 +11,15 @@ $(document).ready(function () {
         });
 
 
-      $('#products').infinitescroll({
-        animate: true,
-        navSelector: ".pagination",
-        nextSelector: ".pagination a[rel=next]",
-        itemSelector: "#products li",
-        loading: {
-          msgText  : "cargando...",
-          finishedMsg: ""
-        }
-      });
+      // $('#products').infinitescroll({
+      //   animate: true,
+      //   navSelector: ".pagination",
+      //   nextSelector: ".pagination a[rel=next]",
+      //   itemSelector: "#products li",
+      //   loading: {
+      //     msgText  : "cargando...",
+      //     finishedMsg: ""
+      //   }
+      // });
 
 });
